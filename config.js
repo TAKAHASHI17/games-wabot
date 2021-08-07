8303ac42f77be5c9569ec17b5d41e73d5fd3ea3c
TAKAHASHI17 <51894208+TAKAHASHI17@users.noreply.github.com>
2021-08-07 16:08:00 +0700

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.owner = ['6281359837041', '6285601993596', '6287888152023']// Put your number here
-global.mods = ['13479805233'] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = ['6281257735703'] // Want some help?
+global.prems = [30 day] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -24,8 +24,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6281359837041'
+global.author = 'Astro Bot Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
 
