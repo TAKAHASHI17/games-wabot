ab8cc90a050e8a2d27d1be517ccdcfdf05adf2f8
TAKAHASHI17 <51894208+TAKAHASHI17@users.noreply.github.com>
2021-08-09 14:53:43 +0700

    config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz' : 'apikeymu' ,
+  'https://api.xteam.xyz' : 'd90a9e986e18778b' ,
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
   'https://pencarikode.xyz': 'pais'
